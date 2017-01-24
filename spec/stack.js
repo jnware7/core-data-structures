f873bde837b6be60433dec463c256195a23f7f66
jnware7 <jnware7@gmail.com>
2017-01-23 17:18:11 -0800

    designed stack tests

--- spec/stack.js
+++ spec/stack.js
@@ -15,8 +15,49 @@ describe('Stack', () => {
     it('pushes an element to the top of the stack.', () => {
       const myStack = new Stack()
 
-      expect(() => myStack.push('foo'))
-        .to.alter(() => myStack.length(), { from: 0, to: 1 })
+      expect(() => myStack.addTop('foo'))
+        .to.alter(() => myStack.lengths(), { from: 0, to: 1 })
+    })
+  })
+
+  context('pop()', () => {
+    it('returns and removes the top element in the stack or null if the stack is empty.', () => {
+      const myStack = new Stack()
+      expect(myStack.removeTop()).to.equal(null);
+    })
+    it('returns and removes the top element in the stack or null if the stack is empty.', () => {
+      const myStack = new Stack()
+        myStack.addTop('value')
+        myStack.addTop('value1')
+        myStack.addTop('value2')
+      expect(myStack.removeTop()).to.equal('value2')
+      })
+    })
+  context('peek()', () => {
+    it('returns the top element in the stack or null if the stack is empty.', () => {
+      const myStack = new Stack()
+      expect(myStack.peek()).to.equal(null);
+      })
+    })
+    it('returns the top element in the stack or null if the stack is empty', () => {
+      const myStack = new Stack()
+        myStack.addTop('value')
+        myStack.addTop('value1')
+        myStack.addTop('value2')
+      expect(myStack.peek()).to.equal('value2')
+      })
+  context('isEmpty',() =>{
+    it('returns true if the stack is empty or false if not.', () => {
+      const myStack = new Stack()
+      myStack.addTop('value')
+      expect(myStack.isEmpty()).to.equal(false)
+    })
+  })
+  context('lengths',() =>{
+    it('returns the number of elements in the stack.', () => {
+      const myStack = new Stack()
+      myStack.addTop('value')
+      expect(myStack.lengths()).to.equal(1)
     })
   })
 })
--- src/stack.js
+++ src/stack.js
@@ -2,4 +2,23 @@
 
 export default class Stack {
   // your code here
+  constructor () {
+    this.array = []
+  }
+  lengths () {
+    return this.array.length
+  }
+  addTop(value) {
+    return this.array.push(value)
+  }
+
+  removeTop(){
+    return this.array.length > 0 ? this.array.pop() : null
+  }
+  peek(){
+    return this.array.length > 0 ? this.array[this.array.length-1] : null
+  }
+  isEmpty(){
+    return this.array.length > 0 ? false : true
+  }
 }
